76e2426d9a5fe23f1c2bbcab30dfd6fce2edc82d
Krissu3212 <80511173+Krissu3212@users.noreply.github.com>
2021-03-12 10:06:40 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+package me.Hullumeelne.playerTeleportProperties;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		System.out.println("playerTeleportProperties has been turned on");
+		
+		this.getCommand("test").setExecutor((CommandExecutor)new TestCmd());
+		
+		PluginManager pm = getServer().getPluginManager();
+		Listeners listener = new Listeners(this);
+		pm.registerEvents(listener, this);
+	}
+	@Override
+	public void onDisable() {
+		System.out.println("playerTeleportProperties has been turned off");
+	}
+}
